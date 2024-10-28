23772506ba49c49b1ace0354ab6ba291bcef5a15
Anna Bevziuk <anna.bevziuk.pp.2022@lpnu.ua>
2024-10-28 17:46:52 +0200

    Add package.json and index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+// Для підтримки JSON-запитів
+app.use(express.json());
+
+// Маршрут для прийому даних від NFC
+app.post('/nfc', (req, res) => {
+    const data = req.body;
+    console.log('Received data:', data);
+    res.send(`Received data: ${JSON.stringify(data)}`);
+});
+
+// Запуск сервера
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
